d519da47f775abb62a2224351d95ce95b2fa0993
Joxan2137 <joxan.discord@gmail.com>
2025-02-08 21:58:00 +0100

    Argument handling structure.

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,120 @@
+use std::path::Path;
+
 use anyhow::Result;
 use clap::{arg, command, Parser};
+use flate2;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None, arg_required_else_help = true)]
 struct Args {
     /// Add files to archive
     #[arg(short, long)]
-    add: bool,
+    add: Option<bool>,
     /// Benchmark
     #[arg(short, long)]
-    benchmark: bool,
+    benchmark: Option<bool>,
     /// Delete files frm archive
     #[arg(short, long)]
-    delete: bool,
+    delete: Option<bool>,
     /// Extract files from archive (without using directory names)
     #[arg(short = 'e', long)]
-    extract: bool,
+    extract: Option<bool>,
     /// Calculate hash values for file
     #[arg(short = 's', long)]
-    hash: bool,
+    hash: Option<bool>,
     /// Show information about supported formats
     #[arg(short, long)]
-    info: bool,
+    info: Option<bool>,
     /// List contents of archive
     #[arg(short, long)]
-    list: bool,
+    list: Option<bool>,
     /// Rename files in archive
     #[arg(short, long)]
-    rename: bool,
+    rename: Option<bool>,
     /// Test integrity of archive
     #[arg(short, long)]
-    test: bool,
+    test: Option<bool>,
     /// Update files to archive
     #[arg(short, long)]
-    update: bool,
+    update: Option<bool>,
     /// Extract files with full paths
     #[arg(short = 'x', long)]
-    full_extract: bool,
+    full_extract: Option<bool>,
     /// Archive name
-    archive_name: String,
+    archive_name: Option<String>,
     /// Files
-    files: Option<Vec<String>>,
+    files: Option<String>,
 }
 
-const SUPPORTED_FORMATS: &[&str] = &[".zip", ".7z"];
+const SUPPORTED_FORMATS: &[&str] = &[".zip", ".7z", ".tar", ".gz", ".rar"];
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    if !args.archive_name.is_ascii() {
-        panic!("non ascii archive name");
-    }
-
-    if !SUPPORTED_FORMATS
-        .iter()
-        .any(|format| args.archive_name.ends_with(format))
-    {
-        panic!("format not supported")
+    if args.archive_name.is_some() {
+        if !SUPPORTED_FORMATS
+            .iter()
+            .any(|format| args.archive_name.as_ref().unwrap().ends_with(format))
+        {
+            panic!("Archive format not supported. Supported Archive formats: {:?}", SUPPORTED_FORMATS);
+        }
     }
 
     Ok(())
+}
+
+fn handle_arg(args: Args) -> Result<()>{
+
+
+    match args {
+        Args { add: Some(true), files: Some(value), .. }  => {
+            let path = Path::new(&value);
+
+            Ok(())
+
+        },   
+        Args { add: Some(true), files: None, .. } => {
+            panic!("A path to the target is required.");
+        },
+        Args { benchmark: Some(true), .. } => {
+            Ok(())
+        },
+
+
+        Args { delete: Some(true), .. } => {
+            Ok(())
+        },
+        Args { extract: Some(true), .. } => {
+            Ok(())
+        },
+
+        Args { hash: Some(true), .. } => {
+            Ok(())
+        },
+
+        Args { info: Some(true), .. } => {
+            Ok(())
+        },
+
+        Args { list: Some(true), .. } => {
+            Ok(())
+        },
+
+        Args { rename: Some(true), .. } => {
+            Ok(())
+        },
+
+        Args { test: Some(true), .. } => {
+            Ok(())
+        },
+        Args { update: Some(true), .. } => {
+            Ok(())
+        },
+        Args { full_extract: Some(true), .. } => {
+            Ok(())
+        },
+        _ => {
+            Ok(())
+        }
+    }
 }
